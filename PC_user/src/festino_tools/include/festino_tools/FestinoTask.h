baf21a58d8b2022f5c37f4b622d1ce8b6003f7cf
MarcoSoriano5102 <marco.soriano5102@comunidad.unam.mx>
2025-07-08 21:52:55 -0600

    Clientes de los task actions creados

---
+++ PC_user/src/festino_tools/include/festino_tools/FestinoTask.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <ros/ros.h>
+#include <actionlib/client/simple_action_client.h>
+#include <festino_task/graspingAction.h>
+#include <festino_task/navigate_to_zoneAction.h>
+#include <festino_task/pose_robot_stationAction.h>
+
+class FestinoTask
+{
+private:
+    static ros::NodeHandle* nh_;
+
+public:
+    static bool setNodeHandle(ros::NodeHandle* nh);
+    static bool grasp(const std::string& action);
+    static bool navigate(const std::vector<std::string>& zones);
+    static bool poseRobot(bool align, const std::string& mps_type, const std::string& mps_band);
+};
---
+++ PC_user/src/festino_tools/src/FestinoTask.cpp
@@ -0,0 +1,70 @@
+#include "festino_tools/FestinoTask.h"
+
+ros::NodeHandle* FestinoTask::nh_ = nullptr;
+
+bool FestinoTask::setNodeHandle(ros::NodeHandle* nh) {
+    nh_ = nh;
+    return nh_ != nullptr;
+}
+
+bool FestinoTask::grasp(const std::string& action) {
+    actionlib::SimpleActionClient<festino_task::graspingAction> client("grasping", true);
+    ROS_INFO("[FestinoTask] Esperando servidor grasping...");
+    client.waitForServer();
+
+    festino_task::graspingGoal goal;
+    goal.action_manip = action;
+
+    client.sendGoal(goal);
+    client.waitForResult(ros::Duration(30.0));
+
+    if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+        ROS_INFO("[FestinoTask] Grasping completado");
+        return client.getResult()->success;
+    } else {
+        ROS_WARN("[FestinoTask] Grasping falló o expiró");
+        return false;
+    }
+}
+
+bool FestinoTask::navigate(const std::vector<std::string>& zones) {
+    actionlib::SimpleActionClient<festino_task::navigate_to_zoneAction> client("zone_navigation", true);
+    ROS_INFO("[FestinoTask] Esperando servidor de navegación...");
+    client.waitForServer();
+
+    festino_task::navigate_to_zoneGoal goal;
+    goal.target_zones = zones;
+
+    client.sendGoal(goal);
+    client.waitForResult(ros::Duration(60.0));
+
+    if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+        ROS_INFO("[FestinoTask] Navegación completada");
+        return client.getResult()->success;
+    } else {
+        ROS_WARN("[FestinoTask] Navegación falló o expiró");
+        return false;
+    }
+}
+
+bool FestinoTask::poseRobot(bool align, const std::string& mps_type, const std::string& mps_band) {
+    actionlib::SimpleActionClient<festino_task::pose_robot_stationAction> client("pose_robot_station", true);
+    ROS_INFO("[FestinoTask] Esperando servidor pose_robot_station...");
+    client.waitForServer();
+
+    festino_task::pose_robot_stationGoal goal;
+    goal.aling = align;
+    goal.mps_type = mps_type;
+    goal.mps_band = mps_band;
+
+    client.sendGoal(goal);
+    client.waitForResult(ros::Duration(30.0));
+
+    if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+        ROS_INFO("[FestinoTask] Pose completado");
+        return client.getResult()->success;
+    } else {
+        ROS_WARN("[FestinoTask] Pose falló o expiró");
+        return false;
+    }
+}
